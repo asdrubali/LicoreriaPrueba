7ab89dda9cdb448b9c393c383e3361b7c972b845
asdrubali <asdrubalinfantepolo@gmail.com>
2023-05-17 03:35:03 -0500

    CTAVAseptima carga - licoreria web V2

--- src/components/Slide.jsx
+++ src/components/Slide.jsx
@@ -5,13 +5,13 @@ export const Slide = () => {
         <div id="carouselExampleSlidesOnly" className="carousel slide" data-bs-ride="carousel">
             <div className="carousel-inner">
               <div className="carousel-item active">
-                <img src="../src/images/imgLicor1.JPG" className="d-block w-100"/>
+                <img src="../images/imgLicor1.JPG" className="d-block w-100"/>
               </div>
               <div className="carousel-item">
-                <img src="../src/images/imgLicor2.JPG" className="d-block w-100"/>
+                <img src="../images/imgLicor2.JPG" className="d-block w-100"/>
               </div>
               <div className="carousel-item">
-                <img src="../src/images/imgLicor3.JPG" className="d-block w-100"/>
+                <img src="../images/imgLicor3.JPG" className="d-block w-100"/>
               </div>
             </div>
         </div>
