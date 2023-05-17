12a6367ddda4a771408cce1fb7ec933fa1e1fd29
asdrubali <asdrubalinfantepolo@gmail.com>
2023-05-17 03:29:43 -0500

    septima carga - licoreria web V2

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,7 +3,7 @@ export const Header = () => {
   return (
     <>
         <div className="logo">
-            <img src="./src/images/logoPrincipal.png" className="img-fluid"/>
+            <img src="../src/images/logoPrincipal.png" className="img-fluid"/>
         </div>
         <hr></hr>
     </>
